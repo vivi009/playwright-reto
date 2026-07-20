e99d8179357f633d5b7a0e3464d2984bcd4092c0
Viviana Lopez <vivislopez9@gmail.com>
2026-07-20 19:21:15 +0200

    Reto 4 Completo

--- tests/navegation.spec.ts
+++ tests/navegation.spec.ts
@@ -64,3 +64,81 @@ test('Check first left menu option is Admin', async ({ page }) => {
     await expect(leftMenuItems.first()).toHaveText('Admin');
 
 });
+
+test('Navigate through the left panel', async({page}) =>{
+
+    // Ir a la aplicación
+    await page.goto('https://opensource-demo.orangehrmlive.com/')
+
+    // Iniciar sesión
+    await page.getByRole('textbox', {name: 'username'}).fill('Admin')
+    await page.getByRole('textbox', {name: 'Password'}).fill('admin123')
+    await page.getByRole('button', {name: 'login'}).click()
+
+    // Verificar que el menú lateral esté visible
+    await expect(page.getByRole('link', {name: 'Admin'})).toBeVisible()
+
+    // Validar los elementos del menu
+
+    const leftMenuItems = page.getByLabel('Sidepanel').getByRole('listitem')
+    const currentMenuItemnsCount =  await leftMenuItems.count()
+
+    for(let i = 0; i<currentMenuItemnsCount; i++){
+
+        const menuItem = leftMenuItems.nth(i)
+        const menuText = await menuItem.innerText()
+
+        console.log('Current menu item', menuText)
+
+        if(menuText !== 'Maintenance'){
+        await menuItem.click()}
+
+    }
+})
+
+test('Navigate through the left panel goBack', async ({ page }) => {
+
+    test.setTimeout(120000);
+    // Ir a la aplicación
+    await page.goto('https://opensource-demo.orangehrmlive.com/');
+
+    // Login
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await expect(page.getByLabel('Sidepanel')).toBeVisible();
+
+    // Contar las opciones del menú una sola vez
+    const menuCount = await page
+        .getByLabel('Sidepanel')
+        .getByRole('listitem')
+        .count();
+
+    for (let i = 0; i < menuCount; i++) {
+
+        // Volver a obtener el locator en cada vuelta
+        const menuItem = page
+            .getByLabel('Sidepanel')
+            .getByRole('listitem')
+            .nth(i);
+
+        const menuText = await menuItem.innerText();
+
+        console.log(`Menú ${i + 1}: ${menuText}`);
+
+        await menuItem.click();
+
+        // Esperar a que cargue la opción
+           await page.waitForLoadState('networkidle');
+
+        if (menuText === 'Maintenance') {
+
+            // Regresar
+            await page.goBack();
+
+            // Esperar a que vuelva el menú
+            await expect(page.getByLabel('Sidepanel')).toBeVisible();
+        }
+    }
+})
